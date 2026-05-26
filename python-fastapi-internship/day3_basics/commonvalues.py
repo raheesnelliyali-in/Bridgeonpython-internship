521fe14a7d041f20e767e38cfd226ffef428f6c7
Muhammed Rahees N <raheesnelliyali01@gmail.com>
2026-05-26 10:35:48 +0530

    Delete python-fastapi-internship/day3_basics directory

--- python-fastapi-internship/day3_basics/commonvalues.py
+++
@@ -1,4 +0,0 @@
-List1=["sahad","rahees","saad","sasil"]
-List2=["rahees","karthik","refai","sahad"]
-common=list(set(List1).intersection(List2))
-print(common)
--- python-fastapi-internship/day3_basics/mergelist.py
+++
@@ -1,8 +0,0 @@
-List1=["sahad","rahees","saad","sasil"]
-List2=["rahees","karthik","refai","sahad"]
-merged=[]
-for name in List1 + List2:
-    if name not in merged:
-        merged.append(name)
-merged.sort()
-print(merged)
