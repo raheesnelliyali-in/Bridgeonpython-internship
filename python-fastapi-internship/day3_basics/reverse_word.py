556386ea1c9812dcca6b748ec23c519c53c366a1
Muhammed Rahees N <raheesnelliyali01@gmail.com>
2026-05-27 09:40:51 +0530

    Add files via upload

---
+++ python-fastapi-internship/day3_basics/reverse_word.py
@@ -0,0 +1,13 @@
+sentence= "Learning is a journey not a race"
+words = sentence.split()
+reversed_words = words[::-1]
+result = " ".join(reversed_words)
+
+print(result)
+# sentence = "python is very easy"
+
+# words = sentence.split()
+# reversed_words = words[::-1]
+# result = " ".join(reversed_words)
+
+# print(result)
---
+++ python-fastapi-internship/day3_basics/wordfrequency.py
@@ -0,0 +1,6 @@
+sentence=("Sahad is a handsome boy")
+word=sentence.split()
+count={}
+for i in word:
+    count[i] = count.get(i, 0) + 1
+print(count)
